5c2070725f292af3bfb363fbe533f799d24b6f09
rikadr <70948427+rikadr@users.noreply.github.com>
2021-10-20 17:47:46 +0200

    Link oppdatert

--- pages/about.js
+++ pages/about.js
@@ -12,7 +12,7 @@ export default function About() {
                 <div className="flex flex-col justify-center items-center h-screen">
                     <h1>Portifolije</h1>
                     <a href="https://fredrikoh92.github.io/Final_Project_18/index.html" className="text-blue-500">SØPL</a>
-                    <a href="gitta.tech" className="text-blue-500">Aqua-Tech</a>
+                    <a href="https://www.gitta.tech" className="text-blue-500">Aqua-Tech</a>
                     <Link href="/"><a className="text-blue-500">Dersom du vil tilbake til hjemmesiden min så kan du klikke på denne setningen (-:</a></Link>
                 </div>
             </main>
