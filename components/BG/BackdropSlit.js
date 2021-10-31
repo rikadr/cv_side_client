a9c8c4b965b2cb2e94c6454dc9dd51c2049d12ba
rikadr <70948427+rikadr@users.noreply.github.com>
2021-10-31 14:56:22 +0100

    intersection fungerer

--- components/BG/BackdropSlit.js
+++ components/BG/BackdropSlit.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect, useRef, useState, useMemo } from "react";
+import UseElementOnScreen from '../hooks/UseElementOnScreen';
+
+
 const BackdropSlit = () => {
+  const targetRef = useRef(null);
+  const isVisible = UseElementOnScreen(
+    {
+      root: null,
+      marginRoot: '0px',
+      threshold: 0.0
+    },
+    targetRef
+  );
+
   return (
-    <div className="backdropSlit">
-        <div className="front animate-flipIn"></div>        
+    <div className="backdropSlit" ref={targetRef}>
+        <div className={`back ${isVisible ? 'animate-fadeIn' : ''}`}></div> 
+        <div className={`front ${isVisible ? 'animate-flipIn' : ''}`}></div> 
     </div>
   );
 };
