07943882c64ae45088338b8baa4804f3b70e899c
rikadr <70948427+rikadr@users.noreply.github.com>
2021-10-31 11:24:23 +0100

    SirkelNavn nå responsive

--- components/SirkelNavn/SirkelNavn.jsx
+++ components/SirkelNavn/SirkelNavn.jsx
@@ -7,10 +7,11 @@ const SirkelNavn = () => {
   return (
     <div className="relative z-40 flex flex-col items-center justify-center min-h-screen py-2 border-2 border-green-400">
       
-      <div className="w-96 h-96 bg-red-200">
-        <div class="bg-yellow-500 drop-shadow-lg rounded-full h-full w-full border-2 border-blue-600">
-          <div className="my-20 border-2 border-green-600">
-            <h1 className="font-titleItal text-lg  leading-12">
+      <div className="w-64 h-64 sm:w-72 sm:h-72 md:w-96 md:h-96">
+        <div class="bg-yellow-500 drop-shadow-lg rounded-full h-full w-full 
+        border-2">
+          <div className="my-10 sm:my-14 md:my-20 mx-6 sm:mx-8 md:mx-12">
+            <h1 className="font-titleItal text-n sm:text-p md:text-u leading-10 sm:leading-11 md:leading-12">
                   Rikard
                 <br /> Noer
                 <br /> Dotzler
--- components/SirkelNavn/SjekkMegUt.jsx
+++ components/SirkelNavn/SjekkMegUt.jsx
@@ -5,8 +5,9 @@ const SjekkMegUt = () => {
     return (
       <div>
           <Link href="/about">
-            <a className="transition duration-300 hover:animate-pulse hover:text-gray-500 text-black-900 text-s font-bread leading-10 hover:font-bold">
-              &#8594; Sjekk meg ut...
+            <a className="transition duration-300 hover:animate-pulse hover:text-gray-500 text-black-900 
+            text-c sm:text-d md:text-e font-bread leading-10 hover:font-bold">
+              &#8594; Sjekk meg ut
             </a>
           </Link>
       </div>
