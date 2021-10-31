d91839e1894d03b957340a9a1d1221b9a2fb91aa
rikadr <70948427+rikadr@users.noreply.github.com>
2021-10-31 13:49:31 +0100

    responsiveness sirkelnavn

--- components/SirkelNavn/SirkelNavn.js
+++ components/SirkelNavn/SirkelNavn.js
@@ -8,7 +8,7 @@ const SirkelNavn = () => {
     <div className="relative z-40 flex flex-col items-center justify-center min-h-screen py-2">
       
       <div className="w-72 h-72 sm:w-96 sm:h-96">
-        <div class="bg-yellow-500 drop-shadow-lg rounded-full h-full w-full 
+        <div class="transition duration-300 ease-in-out bg-yellow-400 hover:bg-red-600 hover:animate-beat drop-shadow-lg rounded-full h-full w-full 
         border-2">
           <div className="my-14 sm:my-20 mx-8 sm:mx-12">
             <h1 className="font-titleItal text-p sm:text-u leading-11 sm:leading-12">
