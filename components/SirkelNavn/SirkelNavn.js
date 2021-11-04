2aa9e440be2e41755060a4fc3955029371a3ea12
rikadr <70948427+rikadr@users.noreply.github.com>
2021-11-04 22:40:44 +0100

    Peder ga meg gud kode

--- components/SirkelNavn/SirkelNavn.js
+++ components/SirkelNavn/SirkelNavn.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { useState } from "react";
 import Link from "next/link";
 import SjekkMegUt from "./SjekkMegUt";
 
 
 const SirkelNavn = () => {
+
+  const Ord = ({tekst1, tekst2}) => {
+    const [visible, setVisible] = useState(false);
+    const toggle = () => {
+      setVisible(!visible);
+    }
+    return (
+      <div onMouseEnter={toggle} onMouseLeave={toggle}>
+        <h1 className="font-titleItal text-p sm:text-u leading-11 sm:leading-12">{visible ? tekst2 : tekst1}</h1>
+      </div>
+    )
+  }
+
   return (
     <div className="relative z-40 flex flex-col items-center justify-center h-screen py-2">
       
@@ -11,11 +25,9 @@ const SirkelNavn = () => {
         <div className="transition duration-300 ease-in-out bg-yellow-400 hover:bg-yellow-500 drop-shadow-lg rounded-full h-full w-full 
         border-2">
           <div className="my-14 sm:my-20 mx-8 sm:mx-12">
-            <h1 className="font-titleItal text-p sm:text-u leading-11 sm:leading-12">
-                  Rikard
-                <br /> Noer
-                <br /> Dotzler
-            </h1>
+            <Ord tekst1="Rikard" tekst2="Designer"/>
+            <Ord tekst1="Noer" tekst2="Web"/>
+            <Ord tekst1="Dotzler" tekst2="Develuper"/>
                 <SjekkMegUt />
           </div>
         </div>
