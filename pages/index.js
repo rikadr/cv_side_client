85d94880f62573c5dc28a93a207088089822607b
rikadr <70948427+rikadr@users.noreply.github.com>
2021-12-21 16:24:16 +0100

    obs tekst

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
       <main className="font-bread">
         <SirkelNavn />
         
-        OBS! Følgende er kun skjermbilder av designet. Implementert design utvikles fortløpende.<br/><br/><br/>
+        OBS! .<br/><br/><br/>
 
         <IntersectEnter>
         <img id="about" src="images/placeholder/About mobile.jpg"  className="visible relative m-auto w-full sm:w-11/12 md:invisible md:absolute "/>
