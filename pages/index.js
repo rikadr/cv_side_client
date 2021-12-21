9f1c0ba84136af3a07e055c279165cf1b0b00f98
rikadr <70948427+rikadr@users.noreply.github.com>
2021-12-21 16:35:13 +0100

    Revert "typo"
    This reverts commit ad5f3ccebe19c23437e8bf133f1245fd93c19b03.

--- pages/index.js
+++ pages/index.js
@@ -25,6 +25,7 @@ export default function Home() {
       <main className="font-bread">
         <SirkelNavn />
         
+<<<<<<< HEAD
         OBS! Følgende er kun skjermbilder av designet. Implementert design utvikles fortløpende.<br/><br/><br/>
 
         <IntersectEnter>
@@ -33,6 +34,48 @@ export default function Home() {
         <IntersectEnter>
         <img src="images/placeholder/About desktop.jpg"  className="invisible absolute m-auto w-full md:visible md:relative lg:w-10/12 xl:w-8/12 2xl:w-1000"/>
         </IntersectEnter>
+=======
+        <div id="about" className="flex flex-wrap flex-row justify-center m-auto w830:w-830 ">
+          <div className="aboutFlexbox md:w-6/12  hidden md:block">
+            <Tile >
+                <img id="about" src="images/placeholder/FullBodyPortrait.jpg" className="hidden m-auto  max-w-5 rounded-full md:block"/>
+            </Tile>
+          </div>
+
+          {/* TILE-FRAME */}  
+          {/* className="flex flex-wrap flex-col justify-between m-auto border-2 border-green-300 w-11/12 sm:w-10/12 md:w-11/12 lg:w-10/12 xl:w-8/12 2xl:w-1000" */}
+
+          <div className="aboutFlexbox flex flex-wrap flex-col w-10/12  md:w-6/12 ">  
+          <TileHeading>
+              About Me
+            </TileHeading>
+              
+            <Tile >
+              <img id="about" src="images/placeholder/TorsoPortrait.jpg" className="block md:hidden m-auto w-11/12 rounded-full "/>
+            </Tile>
+
+            <Tile >
+              <TileAboutYellow>
+                Engasjert og motivert IT-student med bakgrunn i grafisk design.
+              </TileAboutYellow>
+            </Tile>
+
+            <Tile >
+              <TileAbout heading="STUDENT">
+                <p>Studerer informasjonsteknologi (Bachelor) på OsloMet ved siden av diverse nettkurs på front end utvikling. </p>
+                <br/><p>Jeg har også en bachelor som grunnskolelærer i matematikk og naturfag.</p>
+              </TileAbout>
+            </Tile>
+
+            <Tile >
+              <TileAbout heading="FRITID">
+                <p>På siden er jeg aktiv trommeslager i band og musikkprodusent. </p>
+                <br/><p>Jeg har også en bachelor som grunnskolelærer i matematikk og naturfag.</p>
+              </TileAbout>
+            </Tile>          
+          </div>
+        </div>
+>>>>>>> parent of 85dea70 (link og tweaks)
 
         <IntersectEnter>
         <img id="skills" src="images/placeholder/Skills mobile.jpg"  className="visible relative m-auto w-full sm:w-11/12 md:invisible md:absolute "/>
