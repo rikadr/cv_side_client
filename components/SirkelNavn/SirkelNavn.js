4e12beec80bbe3da4d8dd932297b562afff8d237
rikadr <70948427+rikadr@users.noreply.github.com>
2022-01-09 13:20:24 +0100

    updates sirkelnavn

--- components/SirkelNavn/SirkelNavn.js
+++ components/SirkelNavn/SirkelNavn.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import Link from "next/link";
+import IntersectEnter from "../Intersect/IntersectEnter";
 
 const SirkelNavn = () => {
 
@@ -11,8 +12,21 @@ const SirkelNavn = () => {
     }
     return (
       <div onMouseEnter={toggle} onMouseLeave={toggle}>
-        <h1 className={`font-titleItal  leading-12 ${visible ? 'text-white text-r drop-shadow-lg' : 'animate-pulse text-t'}`}>{visible ? tekst2 : tekst1}</h1>
+        <h1 className={`font-titleItal leading-12 ${visible ? 'text-white animate-pulse drop-shadow-lg' : ''}`}>{visible ? tekst2 : tekst1}</h1>
       </div>
+    )
+  }
+
+  const Boble = ({tekst, Bg, url}) => {
+    return (
+      <div className="w-min"><IntersectEnter><Link href={`${url}`}>
+        <a><div className={`
+        w-32 h-32 flex justify-center rounded-full 
+        transform duration-150 text-d hover:text-white hover:font-semibold
+        hover:animate-pulse bg-gradient-to-br ${prop1} ${prop2} backdrop-blur-sm drop-shadow-lg`}>
+            <div className="w-min m-auto">{tekst}</div>
+          </div></a>
+      </Link></IntersectEnter></div>
     )
   }
 
@@ -21,35 +35,18 @@ const SirkelNavn = () => {
     
           <div className="
           flex flex-col justify-center 
-          w-[22rem] h-[25rem] p-10 rounded-full 
-          bg-gradient-to-br from-indigo-500/70 to-pink-600/80 backdrop-blur-sm drop-shadow-lg border-red-500 border-0">
+          w-min text-t
+           border-red-500 border-2">
             <Ord tekst1="Rikard" tekst2="Designer"/>
             <Ord tekst1="Noer" tekst2="Web"/>
             <Ord tekst1="Dotzler" tekst2="Developer"/>
           </div>
 
-          <Link href="/#about">
-            <a className="
-            w-32 h-32 p-7 rounded-full
-            text-d leading-[1.5rem] hover:text-white hover:font-semibold hover:animate-pulse
-            bg-gradient-to-br from-yellow-300/60 to-amber-600/70 backdrop-blur-sm drop-shadow-lg">
-              &#10549; About
-            </a>
-          </Link>
-          
-          <div className="
-          w-32 h-32 p-7 rounded-full
-          text-d leading-[1.5rem]
-          bg-gradient-to-br from-green-300/60 to-green-700/70 backdrop-blur-sm drop-shadow-lg">
-            &#10549; Skills
-          </div>
-
-          <div className="
-          w-32 h-32 px-6 pt-12 rounded-full
-          text-d leading-[1.5rem]
-          bg-gradient-to-br from-red-300/60 to-pink-700/70 backdrop-blur-sm drop-shadow-lg">
-            Works
-          </div>
+          <Boble tekst="About" Bg="from-pink-300/70 to-blue-500/90"  url="/#about" />
+          <Boble tekst="Skills" Bg="from-lime-300/70 to-emerald-500/90"  url="/#skills" />
+          <Boble tekst="Works" Bg="from-teal-300/70 to-emerald-500/90"  url="/works" />
+          <Boble tekst="Works" Bg="from-teal-300/70 to-emerald-500/90"  url="/works" />
+          <Boble tekst="Contact" Bg="from-orange-300/70 to-red-500/90"  url="/404" />
 
       <div className="absolute bottom-20">
         Scroll
